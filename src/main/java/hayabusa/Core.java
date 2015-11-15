4a4b607713b5999799d7da4f3f87384755c38fdb
Hayabusa <hayabusa3430@yahoo.co.jp>
2015-11-15 20:17:02 +0900

    initial commit

---
+++ src/main/java/hayabusa/Core.java
@@ -0,0 +1,21 @@
+package main.java.hayabusa;
+
+import cpw.mods.fml.common.*;
+import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.Mod.*;
+/*
+ * Ó¯Ö‚Ì’è‹`‚È‚Ç
+ */
+@Mod(modid = "Urienmod", name = "Urienmod")
+
+public class Core {
+	
+static void setTextureName(String string){
+}
+	
+
+public void addRecipe(){
+}
+
+
+}
