ce4738634256763294021dc675f1681c12f1802f
'Hayabusa58' <hayabusa3430@yahoo.co.jp>
2015-11-27 18:47:23 +0900

    クラス名を変更

---
+++ src/main/java/hayabusa/UrienModCore.java
@@ -0,0 +1,16 @@
+package hayabusa;
+
+
+import cpw.mods.fml.common.Mod;
+
+/*
+ * Ó¯Ö‚Ì’è‹`‚È‚Ç
+ */
+@Mod(modid = "Urienmod", name = "Urienmod")
+
+public class UrienModCore {
+	
+
+	public static UrienModCore instanse;
+	
+	}
