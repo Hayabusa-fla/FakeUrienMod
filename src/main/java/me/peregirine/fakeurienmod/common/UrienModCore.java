643c2c6f6528d537b40fb82a3557bfcb853b3381
Demitto <satoko.0829@gmail.com>
2015-12-09 00:03:56 +0900

    Update UrienModCore.java

--- src/main/java/me/peregirine/fakeurienmod/common/UrienModCore.java
+++ src/main/java/me/peregirine/fakeurienmod/common/UrienModCore.java
@@ -1,17 +1,20 @@
-package me.peregirine.fakeurienmod;
+package me.peregirine.fakeurienmod.common;
 
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
+import me.peregirine.fakeurienmod.client.ClientSideProxy;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.EnumHelper;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 /*
  * The main class of UrienMod
@@ -19,7 +22,7 @@ import net.minecraftforge.common.util.EnumHelper;
  */
 /*
  * れぎん氏のWiki（Bow)を元にやや大きい変更を行いました＿ぞんび
- * 
+ *
  */
 @Mod(modid = "Urienmod", name = "Urienmod")
 
@@ -27,16 +30,16 @@ public class UrienModCore {
 	//まず不要な@Instanceをコメントアウト
 	//@Instance("Urienmod")
 	//protected static UrienModCore instance;
-	
+
 	//postInitでの謎のItemUrienSwordの処理を削除。＿ぞんび
 	public static Item uriensword;
 	//れぎん氏のWikiを元に攻撃力を設定している。-5.0Fで0を下回っているためノックバックすらない。-4.0Fで通常攻撃力_ぞんび
 	public static final Item.ToolMaterial BLUE = EnumHelper.addToolMaterial("BLUE", 0, 150, -5.0F, -5.0F, 30  );
-	
+
 	@SidedProxy(clientSide = "me.peregirine.fakeurienmod.client.ClientSideProxy", serverSide = "me.peregirine.fakeurienmod.common.CommonSideProxy")
 	public static CommonSideProxy proxy;
 	public static ClientSideProxy clientproxy;
-	
+
 	public static int entityIdHead = 170;
 
 	@Mod.EventHandler
@@ -50,12 +53,12 @@ public class UrienModCore {
 		.setTextureName("urienmod:uriensword");
 		GameRegistry.registerItem(uriensword, "UrienSword");
 	//public static UrienModCore instanse;
-	
+
 	EntityRegistry.registerModEntity(EntityBullet.class, "Arrow",
 				entityIdHead, this, 128, 5, true);
 	proxy.registerRenderers();
 	}
-	
+
 	//RecipeRegister.classでの登録がうまくいっていないようなのでCoreで処理する形に変更＿ぞんび
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event){
@@ -73,6 +76,6 @@ public class UrienModCore {
 			LanguageRegistry.instance().addNameForObject(uriensword, "ja_JP", "宇理炎?");
 
 	}
-	
-	
+
+
 	}
