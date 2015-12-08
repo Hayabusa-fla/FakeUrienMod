9758a65f7071119f22169c47096fc76f8c4136b4
Demitto <satoko.0829@gmail.com>
2015-12-08 23:50:58 +0900

    Create ClientSideProxy.java

---
+++ src/main/java/me/peregirine/fakeurienmod/client/ClientSideProxy.java
@@ -0,0 +1,32 @@
+
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
+public class ClientSideProxy extends CommonSideProxy{
+
+
+	//public static final KeyBinding RReloadKey = new KeyBinding("Key.sample", Keyboard.KEY_R, "MCFireArms");
+	//public static final KeyBinding ADSKey = new KeyBinding("Key.sample2", Keyboard.KEY_LCONTROL, "MCFireArms2");
+	@Override
+	public World getClientWorld()
+	{
+		return FMLClientHandler.instance().getClient().theWorld;
+	}
+
+	@Override
+	public void registerRenderers()
+	{
+		RenderingRegistry.registerEntityRenderingHandler(EntityBullet.class, new EntityRenderer(new BulletModel()));
+
+		//RenderingRegistry.registerEntityRenderingHandler(EntityLaser.class, new EntityRenderer(new LaserModel()));
+	}
+	@Override
+	public boolean isThePlayer(EntityPlayer player)
+	{
+		return player == FMLClientHandler.instance().getClient().thePlayer;
+	}
+
+}
