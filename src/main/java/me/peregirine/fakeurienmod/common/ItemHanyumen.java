8b86f50b80af8757f657e28aa7817357e16f0d4d
Demitto <satoko.0829@gmail.com>
2015-12-11 22:35:18 +0900

    Create ItemHanyumen.java

---
+++ src/main/java/me/peregirine/fakeurienmod/common/ItemHanyumen.java
@@ -0,0 +1,33 @@
+package me.peregirine.fakeurienmod.common;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.world.World;
+
+public class ItemHanyumen extends ItemFood{
+
+	public ItemHanyumen(int p_i45339_1_, float p_i45339_2_, boolean p_i45339_3_) {
+		super(p_i45339_1_, p_i45339_2_, p_i45339_3_);
+		this.setPotionEffect(Potion.harm.id, 30, 10, 1.0F)
+		.setUnlocalizedName("hanyuMen")
+		.setTextureName("urienmod:hanyumen")
+		.setCreativeTab(UrienModCore.tabUrien);
+	}
+	@Override
+	public ItemStack onEaten(ItemStack par1ItemStack, World par2World,
+			EntityPlayer par3EntityPlayer) {
+
+		for (int i = 0; i < 36; i++){
+			 ItemStack item = par3EntityPlayer.inventory.mainInventory[i];
+			 if(item != null)
+				 par3EntityPlayer.inventory.clearInventory(item.getItem(), -1);
+		}
+
+		return par1ItemStack;
+
+	}
+
+
+}
